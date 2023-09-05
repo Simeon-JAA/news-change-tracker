0d05ec37e5cfcaa97d09f434d9fd6df2a837409d
Ieva <ieva.maksvytyte@gmail.com>
2023-09-05 16:07:29 +0100

    more laoding

--- pipeline/load.py
+++ pipeline/load.py
@@ -1,13 +1,15 @@
 """Handles all load functions that load data and information into the database"""
 # pylint: disable=invalid-name
 from os import environ
+import datetime
 import pandas as pd
 from dotenv import load_dotenv
 from psycopg2 import connect
 from psycopg2.extensions import connection
 from psycopg2.extras import execute_values
 
-TRANSFORMED_DATA = ""
+TRANSFORMED_DATA = "transformed_data.csv"
+TIME_NOW = datetime.datetime.now()
 
 def get_db_connection() -> connection:
     """Returns connection to the rds database"""
@@ -33,13 +35,13 @@ def get_data_from_db(conn: connection, table: str, column = "*")-> pd.DataFrame:
     return pd.DataFrame(result)
 
 
-def check_for_duplicates(conn: connection, df: pd.DataFrame, table: str, column: str) \
+def check_for_duplicates(conn: connection, df: pd.DataFrame, table: str, df_column: str, db_column) \
     -> pd.DataFrame:
     """Checks a local dataframe against a database table on specific column.
     Returns non-duplicative local data only"""
     df = df.copy()
     articles = get_data_from_db(conn, table)
-    df = df[not df[column].isin(articles[column])]
+    df = df[not df[df_column].isin(articles[db_column])]
     # test this line
 
     return df
@@ -66,16 +68,19 @@ def add_to_scraping_info_table_from_pandas(conn: connection, df: pd.DataFrame) -
                        article_id) VALUES %s""", tuples)
         cur.commit()
 
-# hardcode
+# hardcode source
 
 def add_to_authors_table_from_pandas(conn: connection, df: pd.DataFrame) -> None:
     """Converts df into tuples, then adds to authors table.
     NB: needs article_id column converted into foreign key reference"""
     pass
 
+
 if __name__ == "__main__":
     db_conn = get_db_connection()
     df_transformed = pd.read_csv(TRANSFORMED_DATA)
+    print(df_transformed)
 
-    df_no_duplicates = check_for_duplicates(db_conn, df_transformed, "article", "article_url")
+
+    df_no_duplicates = check_for_duplicates(db_conn, df_transformed, "article", "url", "article_url")
     db_conn.close()
