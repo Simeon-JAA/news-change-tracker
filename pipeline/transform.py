6df347ae0f2287be7a36922a82d73921707fac8a
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-09-05 13:05:18 +0100

    1st completion of transform.py

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -3,15 +3,20 @@
 import pandas as pd
 from pandas import DataFrame
 
+
 def get_rss_feed_df(file_path: str) -> DataFrame:
     """Returns the scraped article csv as a DataFrame"""
+
     df = pd.read_csv(file_path)
+
     return df
 
 
 def get_scraped_articles_df(file_path: str) -> DataFrame:
     """Returns the rss_feed csv as a DataFrame"""
+
     df = pd.read_csv(file_path)
+
     return df
 
 
@@ -23,6 +28,7 @@ def format_time_to_timestamp(time_in_col: str) -> str:
                         "jul": "07", "aug": "08", "sep": "09",
                         "oct": "10", "nov": "11", "dec": "12",
                         }
+    
     time_in_col = time_in_col.split(" ")
     time_in_col = time_in_col[1:]
 
@@ -35,22 +41,62 @@ def format_time_to_timestamp(time_in_col: str) -> str:
     if time_in_col[-1].upper()  == "GMT":
          date = "-".join(time_in_col[:-2])
          time = time_in_col[-2]
+
          return f"{date} {time}"
-    return 
+    #TODO what if time is not GMT?
+    
+    return
 
 
 def format_rss_feed_df(df: DataFrame) -> DataFrame:
     """Formats rss feed dataframe columns before saving to csv df"""
 
     df["published"] = df["published"].apply(format_time_to_timestamp)
-    df["id"] = df["id"].apply(lambda row: row.strip())
+    df["id"] = df["id"].apply(lambda url: url.strip())
     df = df[["id", "title", "published"]]
 
     return df
 
 
+def format_authors(authors: str) -> str | None :
+    """Returns a list of authors if they exist, and none if they dont"""
+
+    if authors.lower() == "nan":
+        return None
+    
+    if authors[:3].lower() == "by ":
+        authors = authors.lower().replace("by ", "").replace(" &", ",")
+        authors = authors.replace(" and", ",")
+        authors = authors.split(", ")
+
+        authors = list(map(lambda author: author.title(), authors))
+
+        return authors
+
+
+def get_source(source: str) -> str:
+    """Returns source for given input"""
+
+    sources = {"bbc": "BBC",
+               "thesundaily": "The Sun"}
+    
+    for possible_source in list(sources.keys()):
+        if possible_source in source:
+            return sources[possible_source]
+    
+    
+    return "Unknown"
+
+
 def format_scraped_articles_df(df: DataFrame) -> DataFrame:
     """Formats scraped_articles dataframe columns before saving to csv df"""
+
+    df["url"] = df["url"].apply(lambda url: url.strip())
+    df["source"] = df["url"]
+    df["source"] = df["source"].apply(get_source)
+
+    df["author"] = df["author"].apply(lambda authors: str(authors))
+    df["author"] = df["author"].apply(format_authors)
 
     return df
 
@@ -62,13 +108,18 @@ if __name__ == "__main__":
     rss_feed_df = format_rss_feed_df(rss_feed_df)
 
     scraped_article_df = get_scraped_articles_df("scraped_articles.csv")
+    
+    scraped_article_df = format_scraped_articles_df(scraped_article_df)
 
-    print(scraped_article_df.columns)
 
-    # joined_data = pd.merge(left=scraped_article_df,
-    #                         right=rss_feed_df,
-    #                         left_on="url",
-    #                         right_on="id")
+    joined_data = pd.merge(left=scraped_article_df,
+                            right=rss_feed_df,
+                            left_on="url",
+                            right_on="id", 
+                            how="inner")
+    
+    
+    joined_data = joined_data[["body, headline, source, url, author, title, published"]]
 
-    # print(joined_data.columns)
+    joined_data.to_csv("transformed_data.csv")
 
