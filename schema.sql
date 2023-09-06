6987b70f35cabcc91c0483876fa6c23f8cd3555d
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-09-06 16:48:05 +0100

    Update scraping_info table name

--- schema.sql
+++ schema.sql
@@ -28,13 +28,13 @@ CREATE TABLE IF NOT EXISTS article_author (
     FOREIGN KEY (author_id) REFERENCES author(author_id)
 );
 
-CREATE TABLE IF NOT EXISTS scraping_info (
-    scraping_info_id INT GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE IF NOT EXISTS article_version (
+    article_version_id INT GENERATED ALWAYS AS IDENTITY,
     scraped_at TIMESTAMPTZ NOT NULL,
     heading TEXT NOT NULL,
     body TEXT NOT NULL,
     article_id INT NOT NULL,
-    PRIMARY KEY(scraping_info_id),
+    PRIMARY KEY(article_version_id),
     FOREIGN KEY (article_id) REFERENCES article(article_id),
     CONSTRAINT check_scraped_at CHECK (scraped_at <= NOW())
 );
