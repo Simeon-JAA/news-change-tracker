d648efb3b18476b5af48d1b4b65a0e9bc8558296
Ieva <ieva.maksvytyte@gmail.com>
2023-09-07 11:55:04 +0100

    added a try-except to load

--- pipeline/main.py
+++ pipeline/main.py
@@ -2,9 +2,12 @@
 
 from extract import extract_data
 from transform import transform_data
+from load import load_data
 
 if __name__ == "__main__":
 
     extract_data()
 
     transform_data()
+
+    load_data()
