6ec406fd936c4244803955eaec7ee7af63157cda
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-09-08 10:40:01 +0100

    Autopep 8 format on close

--- pipeline/load.py
+++ pipeline/load.py
@@ -18,10 +18,10 @@ def get_db_connection() -> connection:
     load_dotenv()
     try:
         return connect(host=environ["DB_HOST"],
-                   user=environ["DB_USER"],
-                   password=environ["DB_PASSWORD"],
-                   port=environ["DB_PORT"],
-                   dbname=environ["DB_NAME"])
+                       user=environ["DB_USER"],
+                       password=environ["DB_PASSWORD"],
+                       port=environ["DB_PORT"],
+                       dbname=environ["DB_NAME"])
     except Exception as exc:
         print(f"get_db_connection Error: {exc}")
 
@@ -87,7 +87,8 @@ def retrieve_author_id(conn: connection, name: str) -> str:
     """Retrieves author_id from author table."""
 
     with conn.cursor() as cur:
-        cur.execute("""SELECT author_id from author where author_name = %s""", [name])
+        cur.execute(
+            """SELECT author_id from author where author_name = %s""", [name])
         author_id = cur.fetchall()
         if len(author_id) == 1:
             return author_id[0][0]
@@ -98,7 +99,8 @@ def retrieve_article_id(conn: connection, url: str) -> str:
     """Retrieves article_id from author table."""
 
     with conn.cursor() as cur:
-        cur.execute("""SELECT article_id from article where article_url = %s""", [url])
+        cur.execute(
+            """SELECT article_id from article where article_url = %s""", [url])
         url = cur.fetchall()
         if len(url) == 1:
             return url[0][0]
@@ -111,7 +113,8 @@ def add_to_author_table(conn: connection, df: pd.DataFrame) -> None:
 
     with conn.cursor() as cur:
         tuples = df.to_records(index=False)
-        execute_values(cur, """INSERT INTO author (author_name) VALUES %s;""", tuples)
+        execute_values(
+            cur, """INSERT INTO author (author_name) VALUES %s;""", tuples)
         conn.commit()
 
 
@@ -133,41 +136,45 @@ def load_data():
         df_transformed = pd.read_csv(TRANSFORMED_DATA)
 
         # removes duplicates
-        df_transformed["url"] = df_transformed["url"].apply\
-            (lambda x: check_for_duplicate_articles(db_conn, x))
+        df_transformed["url"] = df_transformed["url"].apply(
+            lambda x: check_for_duplicate_articles(db_conn, x))
         df_transformed = df_transformed.dropna(subset=["url"])
 
         # inserts articles
         df_for_article = df_transformed[["url", "published"]].copy()
         df_for_article["source"] = "BBC"
-        df_for_article = df_for_article.reindex(columns=["url", "source", "published"])
+        df_for_article = df_for_article.reindex(
+            columns=["url", "source", "published"])
         add_to_article_table(db_conn, df_for_article)
 
         # inserts authors
         df_for_author = df_transformed[["author"]].copy().dropna()
-        df_for_author["author"] = df_for_author["author"].apply(lambda x: re.findall("'([^']*)'", x))
+        df_for_author["author"] = df_for_author["author"].apply(
+            lambda x: re.findall("'([^']*)'", x))
         df_for_author = df_for_author.explode(column=["author"]).dropna()
         df_for_author = df_for_author.drop_duplicates(subset=["author"])
-        df_for_author["author"] = df_for_author["author"].apply\
-            (lambda x: check_for_duplicate_authors(db_conn, x))
+        df_for_author["author"] = df_for_author["author"].apply(
+            lambda x: check_for_duplicate_authors(db_conn, x))
         df_for_author = df_for_author.dropna()
         add_to_author_table(db_conn, df_for_author)
 
         # inserts article-author table entries
         df_author_article = df_transformed[["url", "author"]].copy().dropna()
-        df_author_article["author"] = df_author_article["author"].apply\
-            (lambda x: re.findall("'([^']*)'", x))
-        df_author_article = df_author_article.explode(column=["author"]).dropna()
-        df_author_article["url"] = df_author_article["url"].apply\
-            (lambda x: retrieve_article_id(db_conn, x)).map(str)
-        df_author_article["author"] = df_author_article["author"].apply\
-            (lambda x: retrieve_author_id(db_conn, x)).map(str)
+        df_author_article["author"] = df_author_article["author"].apply(
+            lambda x: re.findall("'([^']*)'", x))
+        df_author_article = df_author_article.explode(
+            column=["author"]).dropna()
+        df_author_article["url"] = df_author_article["url"].apply(
+            lambda x: retrieve_article_id(db_conn, x)).map(str)
+        df_author_article["author"] = df_author_article["author"].apply(
+            lambda x: retrieve_author_id(db_conn, x)).map(str)
         add_to_article_author_table(db_conn, df_author_article)
 
         # insert article_version
-        df_for_version = df_transformed[["published", "title", "body", "url"]].copy()
-        df_for_version["url"] = df_for_version["url"].apply\
-            (lambda x: retrieve_article_id(db_conn, x)).map(str)
+        df_for_version = df_transformed[[
+            "published", "title", "body", "url"]].copy()
+        df_for_version["url"] = df_for_version["url"].apply(
+            lambda x: retrieve_article_id(db_conn, x)).map(str)
         df_for_version["published"] = str(TIME_NOW)
         add_to_article_version_table(db_conn, df_for_version)
     except KeyboardInterrupt:
