9dfb0ec859fc60b390fde206dff8072964a30f8f
Ieva <ieva.maksvytyte@gmail.com>
2023-09-11 21:20:10 +0100

    making headway

--- dashboard/main.py
+++ dashboard/main.py
@@ -2,10 +2,12 @@
 
 from os import environ
 from dotenv import load_dotenv
+import pandas as pd
 from psycopg2 import connect
 from psycopg2.extensions import connection
 import streamlit as st
 from annotated_text import annotated_text
+
 
 def get_db_connection() -> connection:
     """Returns connection to the rds database"""
@@ -18,6 +20,9 @@ def get_db_connection() -> connection:
                        port=environ["DB_PORT"],
                        dbname=environ["DB_NAME"])
 
+
+def retrieve_article_info() -> pd.DataFrame:
+    """Retrieves all article information. Returns dataframe"""
 
 def highlighted_text() -> None:
     """Displays highlighted changes side by side"""
@@ -54,6 +59,7 @@ def display() -> None:
     """Displays the dashboard"""
 
     highlighted_text()
+
 
 if __name__ == "__main__":
     display()
