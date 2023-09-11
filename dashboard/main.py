6734522b1e5843253c5b7752b6e5eba59172e163
Ieva <ieva.maksvytyte@gmail.com>
2023-09-11 15:57:16 +0100

    some dashboard

--- dashboard/main.py
+++ dashboard/main.py
@@ -4,7 +4,8 @@ from os import environ
 from dotenv import load_dotenv
 from psycopg2 import connect
 from psycopg2.extensions import connection
-
+import streamlit as st
+from annotated_text import annotated_text
 
 def get_db_connection() -> connection:
     """Returns connection to the rds database"""
@@ -18,10 +19,41 @@ def get_db_connection() -> connection:
                        dbname=environ["DB_NAME"])
 
 
+def highlighted_text() -> None:
+    """Displays highlighted changes side by side"""
+
+    changes = """ I
++don't
++know
++if
++I
++like
++or
++not
+like
+tomatoes
+and
+fruits
+-jcgjcgcjgjcg"""
+    string_builder = []
+    changes = changes.split('\n')
+    print(changes)
+    for word in changes:
+        if word[0] == '+':
+            word = word.replace("+", " ")
+            string_builder.append((word, "", "#81d654"))
+        elif word[0] == '-':
+            word = word.replace("-", " ")
+            string_builder.append((word, "", "#f07ff0"))
+        else:
+            string_builder.append((" " + word))
+    annotated_text(string_builder)
+
+
 def display() -> None:
     """Displays the dashboard"""
 
-
+    highlighted_text()
 
 if __name__ == "__main__":
     display()
